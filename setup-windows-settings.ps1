b778fa40f3ef66985db32ae740c8e40c5b11635b
yihou <looyihou91@gmail.com>
2025-02-21 15:51:20 +0800

    move more configs to reg file

--- setup-windows-settings.ps1
+++ setup-windows-settings.ps1
@@ -3,38 +3,6 @@ Set-ItemProperty -Path "HKCU:\Control Panel\Colors" -Name "Background" -Value "0
 Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "Wallpaper" -Value ""
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
-# Enable Dark Theme
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0 -PropertyType DWord -Force
-
-# Disable "Get fun facts, tips, tricks on lock screen"
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "RotatingLockScreenOverlayEnabled" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager" -Name "SubscribedContent-338393Enabled" -Value 0 -PropertyType DWord -Force
-
-# Set Lock Screen Status to None
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Lock Screen" -Name "LockScreenStatus" -Value "" -PropertyType String -Force
-
-# Set Touch Keyboard size to Largest & Key Text size to Medium
-New-ItemProperty -Path "HKCU:\Software\Microsoft\TabletTip\1.7" -Name "KeyboardScaleFactor" -Value 150 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\TabletTip\1.7" -Name "TextSize" -Value 1 -PropertyType DWord -Force
-
-# Uncheck All Options in Start Menu
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackProgs" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_TrackDocs" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Start_PromotedItemsEnabled" -Value 0 -PropertyType DWord -Force
-
-# Set Taskbar - Search Icon Only, Disable Task View & Widgets, Enable Touch Keyboard Always
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 1 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ShowTaskViewButton" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0 -PropertyType DWord -Force
-New-ItemProperty -Path "HKCU:\Software\Microsoft\TabletTip\1.7" -Name "EnableDesktopModeAutoInvoke" -Value 1 -PropertyType DWord -Force
-
-# Set Display Scaling to 125%
-New-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "LogPixels" -Value 120 -PropertyType DWord -Force
-
-# Turn Off Notifications
-New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Value 0 -PropertyType DWord -Force
-
 # Set Power Options - Disable Sleep, Hibernate, and Screen Timeout
 powercfg -change -monitor-timeout-ac 0
 powercfg -change -monitor-timeout-dc 0
